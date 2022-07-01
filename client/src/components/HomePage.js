47ad3a10853263a96cbe20c7fcd4a57c1c0c386a
murphy-mi <murphymax1@gmail.com>
2022-06-30 21:49:16 -0400

    Removing API call for testing

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 
 function HomePage() {
 
-    useEffect(() => {
-        fetch("https://www.googleapis.com/books/v1/volumes?q=education+subject&maxResults=3&key=AIzaSyD2iMrcxWXeVK4l3X93uzwsEtrvDaUzLXE")
-            .then(res => res.json())
-            .then(console.log)
-    }, [])
+    // useEffect(() => {
+    //     fetch("https://www.googleapis.com/books/v1/volumes?q=education+subject&maxResults=3&key=AIzaSyD2iMrcxWXeVK4l3X93uzwsEtrvDaUzLXE")
+    //         .then(res => res.json())
+    //         .then(console.log)
+    // }, [])
 
     return (
         <h3>HomePage goes here</h3>
